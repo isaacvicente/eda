1bf66d2d71ab59da93842efdf38468daec1cdc7c
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-08-20 18:04:56 -0300

    Resolve

--- melhorPivot.java
+++ melhorPivot.java
@@ -14,42 +14,30 @@ class melhorPivot {
         int c1 = candidatos[0];
         int c2 = candidatos[1];
 
-        int i1 = 0;
-        int i2 = 0;
-        int j1 = 0;
-        int j2 = 0;
+        System.out.println(melhorPivot(array, c1, c2));
+    }
+
+    private static int melhorPivot(int[] array, int i, int j) {
+        int countI = 0;
+        int countJ = 0;
 
         for (int k = 0; k < array.length; k++) {
-            if (k != c1) {
-                if (array[k] < array[c1])
-                    i1++;
-                else if (array[k] > array[c1])
-                    j1++;
-                else {
-                    if (k < c1)
-                        i1++;
-                    else
-                        j1++;
-                }
+            if (array[k] < array[i]) {
+                countI++;
+            } else if (array[k] > array[i]) {
+                countI--;
             }
-
-            if (k != c2) {
-                if (array[k] < array[c2])
-                    i2++;
-                else if (array[k] > array[c2])
-                    j2++;
-                else {
-                    if (k < c2)
-                        i2++;
-                    else
-                        j2++;
-                }
+            if (array[k] < array[j]) {
+                countJ++;
+            } else if (array[k] > array[j]) {
+                countJ--;
             }
         }
 
-        if (Math.abs(i1 - j1) <= Math.abs(i2 - j2))
-            System.out.println(c1);
-        else
-            System.out.println(c2);
+        if (Math.abs(countI) <= Math.abs(countJ)) {
+            return i;
+        } else {
+            return j;
+        }
     }
 }
