58ec4e9717ecea40b8fe5162efd3a791e5f1a625
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-09-03 14:40:05 -0300

    Faz com que `invertePilhaIndex` use apenas pilhas

--- tst/invertePilhaIndex.java
+++ tst/invertePilhaIndex.java
@@ -13,23 +13,28 @@ class invertePilhaIndex {
 
     int ind = Integer.parseInt(sc.nextLine());
 
-    for (int i = 0; i < capacidade; i++) {
+    for (int i = capacidade - 1; i >= 0; i--) {
       pilha.push(array[i]);
     }
 
-    int[] aux = new int[ind + 1];
+    Pilha aux = new Pilha(capacidade);
+    Pilha result = new Pilha(capacidade);
 
-    for (int j = 0; j <= ind; j++) {
-      aux[j] = pilha.pop();
+    for (int j = 0; j < capacidade; j++) {
+      if (j < capacidade - 1 - ind) {
+        result.push(pilha.pop());
+      } else {
+        aux.push(pilha.pop());
+      }
     }
 
     for (int k = 0; k <= ind; k++) {
-      pilha.push(aux[k]);
+      result.push(aux.pop());
     }
 
     System.out.println("-");
     for (int i = 0; i < capacidade; i++) {
-      System.out.println(pilha.pop());
+      System.out.println(result.pop());
     }
 
     sc.close();
